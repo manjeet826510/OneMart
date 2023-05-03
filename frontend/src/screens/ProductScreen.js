bc963c9311a02048a2d0e29ee6fd18d1d3959e9f
Manjeet Kumar <manjeet826510@gmail.com>
2023-05-04 02:58:03 +0530

    Implementing Add to Cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useReducer } from 'react'
+import React, { useContext, useEffect, useReducer } from 'react'
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -12,6 +12,7 @@ import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import getError from '../utils';
+import { Store } from '../Store';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -49,6 +50,11 @@ const ProductScreen = () => {
       };
       fetchData();
     }, [slug]);
+
+    const {state, dispatch: ctxDispatch} = useContext(Store)
+    const addToCartHandler = ()=>{
+      ctxDispatch({type: 'CART_ADD_ITEM', payload: {...product, /*quantity: 1*/}});
+    }
   return (
     loading ? (
       <LoadingBox/>
@@ -102,7 +108,7 @@ const ProductScreen = () => {
                 product.countInStock>0 && (
                   <ListGroup.Item>
                     <div className='d-grid'>
-                      <Button variant='primary'>Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant='primary'>Add to Cart</Button>
                     </div>
                   </ListGroup.Item>
                 )
