fc7c7d532ab22b2eaeb6020d69fd542c301285a6
Manjeet Kumar <manjeet826510@gmail.com>
2023-05-12 13:15:49 +0530

    25. Create Sign Up Screen

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -29,4 +29,25 @@ userRouter.post(
   })
 );
 
+userRouter.post(
+  "/signup",
+  expressAsyncHandler(async (req, res) => {
+    // console.log(req.body);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    // console.log(user);
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      jwtToken: generateToken(user),
+    });
+  })
+);
+
 export default userRouter;
